db572584a2a76f65a39879d58e2f0d40fa2efc6c
Tvli <526864480@qq.com>
2014-09-04 14:07:07 +0100

    uri validation added

--- tasks/http_resource.rb
+++ tasks/http_resource.rb
@@ -5,13 +5,23 @@ require_relative '../modules/dashboard.rb'
 class HttpResource
 
   def fetch(url)
-    uri = URI(url)
+    uri = uri(url)
     begin
       response = Net::HTTP.get_response(uri)
-    rescue => e
-      raise Dashboard::InvalidEndpointError, e.backtrace
+    rescue
+      raise Dashboard::InvalidEndpointError
     end
     response.code == "200" ? response : false
+  end
+
+private
+  def uri(url)
+    uri = URI(url)
+    begin
+      uri.scheme ? uri : false
+    rescue
+      raise URI::InvalidURIError
+    end
   end
 
 end
