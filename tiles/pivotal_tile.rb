cf2cfe4753c4609ef8c7908613c3222765e384c7
Tvli <526864480@qq.com>
2014-09-25 16:56:29 +0100

    Grouped stories into collapsible tree by state or labels

--- tiles/pivotal_tile.rb
+++ tiles/pivotal_tile.rb
@@ -6,6 +6,7 @@ class PivotalTile < JSONTile
   attr_accessor :objects
   attr_accessor :type
   attr_accessor :stories_for_owners
+  attr_accessor :grouped_stories
 
   def update
     headers = {"X-TrackerToken" => @token.to_s}
@@ -24,8 +25,10 @@ class PivotalTile < JSONTile
     when "members_stories"
       calculate_stories_for_owners  
       context = {:type => @type, :stories_for_owners => @stories_for_owners}
-    when "stories_by_status"
-      context = {:objects => @objects, :type => @type}
+    when "stories_by_state"
+      @grouped_stories = []
+      classify_stories_by_state
+      context = {:objects => @objects, :type => @type, :grouped_stories => @grouped_stories}
     when "json_format"
       context = {:objects => @objects, :type => @type}
     end
@@ -55,4 +58,51 @@ class PivotalTile < JSONTile
       @stories_for_owners.push({"owner_id"=> owner_id, "story_amount"=> counter}) 
     end
   end
+
+  def classify_stories_by_state
+    @objects.each do |items|
+      if items.has_key?("current_state") && items.has_key?("name")
+        @grouped_stories.push(item(items["current_state"], items["name"], "state"))
+      end
+
+      if items.has_key?("labels") && items.has_key?("name")
+        items["labels"].each do |label|
+          @grouped_stories.push(item(label["name"], items["name"], "labels"))
+        end
+      end
+    end
+  end
+
+  def item(current_status, name, option)
+    if option.eql?("state")
+      { "category"=> "Grouped by state", "status"=> current_status , "name"=> name }
+    elsif option.eql?("labels")
+      { "category"=> "Grouped by labels", "status"=> current_status , "name"=> name }
+    else
+      return
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
